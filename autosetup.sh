d8b2956ae0becff3f208b9eb32f3cd8cc7436fa0
ZusorCode <tobias.d.messner@gmail.com>
2018-02-07 16:37:40 -0700

    Update autosetup.sh

--- autosetup.sh
+++ autosetup.sh
@@ -19,12 +19,9 @@ else
     echo "Strongly recommended not to continue!!!"
     echo "Please change the path of the location in this script!"
 fi
-
-read -p "Are you sure? " -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+ead -p "Continue (y/n)?" CONT
+if [ "$CONT" = "y" ]; then
+  [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
     echo "I warned you!"
     echo "Installing gnash"
     sudo apt-get gnash
@@ -35,6 +32,8 @@ then
     sudo cat "$NEWCFG" >> "$EXISTINFCFG"
     echo "Creating Rom Folder"
     mkdir /home/pi/RetroPie/roms/nes
-    
+else
+  echo "Cancelled"
 fi
+
 echo "Done"
