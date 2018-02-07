e36488df942d1f556e1d853fe5e0a5bfb6faab63
ZusorCode <tobias.d.messner@gmail.com>
2018-02-07 13:39:59 -0700

    Update autosetup.sh

--- autosetup.sh
+++ autosetup.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
-NEWCFG=
+echo "You should edit the new_es_systems.cfg to use the path where you want to store your swf files!"
+NEWCFG=new_es_systems.cfg
 EXISTINGCFG=/etc/emulationstation/es_systems.cfg
+
 read -p "This script has not been tested! Are you sure you want to do this? " -n 1 -r
 echo    
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
     [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+    echo "I warned you!"
+    echo "Installing gnash"
     sudo apt-get gnash
+    echo "Making Backup of default es_systems.cfg at /opt/retropie/configs/all/emulationstation/es_systems.cfg"
     sudo cp /etc/emulationstation/es_systems.cfg /opt/retropie/configs/all/emulationstation/es_systems.cfg.
-    cat "$NEWCFG" >> "$EXISTINFCFG"
+    echo "Adding the player to "
+    sudo cat "$NEWCFG" >> "$EXISTINFCFG"
+    echo
 fi
 
 
