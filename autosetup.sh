fb88d229bd1e13d1fa3f15d010b1f01f39109639
ZusorCode <tobias.d.messner@gmail.com>
2018-02-07 13:32:54 -0700

    Update autosetup.sh

--- autosetup.sh
+++ autosetup.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
-sudo apt-get gnash
-sudo cp /etc/emulationstation/es_systems.cfg /opt/retropie/configs/all/emulationstation/es_systems.cfg.
+NEWCFG=
+EXISTINGCFG=/etc/emulationstation/es_systems.cfg
+read -p "This script has not been tested! Are you sure you want to do this? " -n 1 -r
+echo    
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+    sudo apt-get gnash
+    sudo cp /etc/emulationstation/es_systems.cfg /opt/retropie/configs/all/emulationstation/es_systems.cfg.
+    cat "$NEWCFG" >> "$EXISTINFCFG"
+fi
+
+
