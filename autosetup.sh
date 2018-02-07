73c2d16efa65077d41b14edf599cc3077f413823
ZusorCode <tobias.d.messner@gmail.com>
2018-02-07 16:35:47 -0700

    Update autosetup.sh

--- autosetup.sh
+++ autosetup.sh
@@ -20,10 +20,9 @@ else
     echo "Please change the path of the location in this script!"
 fi
 
-
-read -p "This script has not been tested! Are you sure you want to do this? " -n 1 -r
-echo    
-if [[ ! $REPLY =~ ^[Yy]$ ]]
+read -p "Are you sure? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
 then
     [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
     echo "I warned you!"
@@ -36,5 +35,6 @@ then
     sudo cat "$NEWCFG" >> "$EXISTINFCFG"
     echo "Creating Rom Folder"
     mkdir /home/pi/RetroPie/roms/nes
+    
 fi
 echo "Done"
